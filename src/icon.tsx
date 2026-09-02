d9d2232c116f834c62b8a547a6782f3cf3654a89
anthonyrocha-collab <anthony.rocha@acad.ufsm.br>
2026-09-02 11:59:49 -0300

    Delete src/icon.tsx

--- src/icon.tsx
+++
@@ -1,3 +0,0 @@
-git add src/utils/icons.tsx src/services/googleFonts.ts src/services/theme.ts src/components/admin/AdminAppearanceTab.tsx
-git commit -m "Add portfolio icon registry"
-git push origin main
