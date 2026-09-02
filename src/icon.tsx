6f1d0681aecefa030a3d0d63d82b34ae18c20186
anthonyrocha-collab <anthony.rocha@acad.ufsm.br>
2026-09-02 11:55:52 -0300

    Add portfolio icon registry

---
+++ src/icon.tsx
@@ -0,0 +1,3 @@
+git add src/utils/icons.tsx src/services/googleFonts.ts src/services/theme.ts src/components/admin/AdminAppearanceTab.tsx
+git commit -m "Add portfolio icon registry"
+git push origin main
